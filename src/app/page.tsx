edce69fcf55be0e975484103855f0182662a844c
OHkorapan <koraphan.muangsong@onestudentportal.com>
2025-07-27 13:58:33 +0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import DrawingBoard from '../component/DrawingBoard';
 export default function DrawPage() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">🖌️ กระดานวาดภาพ</h1>
+      <h1 className="text-2xl font-bold mb-4">🖌️ กระดานวาดภาพนะครับ</h1>
       <DrawingBoard />
     </div>
   );
